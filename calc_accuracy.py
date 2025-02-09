4a98933987fae0d1195dffe1bf633762d92bd302
GongyeLiu <gongyeliuu@gmail.com>
2025-02-09 18:11:26 +0800

    fix bug

--- calc_accuracy.py
+++ calc_accuracy.py
@@ -1,6 +1,22 @@
 def suff_stats(h, m, epsilon):
+    """
+    +------------+-----------+-----------+-----------+
+    | Notation   |          Model Prediction         |
+    |            |     <     |     =     |     >     |
+    +------------+-----------+-----------+-----------+
+    |  Human   < |     C     |     Tm    |     D     |
+    |  Label   = |     Th    |    Thm    |     Th    |
+    |          > |     D     |     Tm    |     C     |
+    +------------+-----------+-----------+-----------+
+    C: Consistent on the preference,
+    D: Discordant on the preference,
+    Th: Human ties but model doesn't,
+    Tm: Model ties but human doesn't,
+    Thm: Both human and model ties,
+    epsilon: threshold for ties
+    """
     C = D = Th = Tm = Thm = 0
-    n = len(h)
+
     for hi, mi in zip(h, m):
         if hi == 0 and abs(mi) <= epsilon:
             Thm += 1
@@ -14,9 +30,8 @@ def suff_stats(h, m, epsilon):
             D += 1
     return C, D, Th, Tm, Thm
 
-def calc_tau(C, D, Th, Tm, Thm):
-    # This function calculates the current tau value based on the statistics
-    # You can define your own logic here based on how tau is calculated in your context
+def calc_acc(C, D, Th, Tm, Thm):
+    # This function calculates the current accuracy based on the statistics
     return (C + Thm) / (C + D + Th + Tm + Thm)
 
 def calc_accuracy_with_ties(h, m):
@@ -24,66 +39,84 @@ def calc_accuracy_with_ties(h, m):
     algorithm: https://arxiv.org/abs/2305.14324
     O(N^2logN)
     Input:
-        h: list of N human labels, 1 for preferred, -1 for not preferred, 0 for ties
-        m: list of N model scores(scores_A - scores_B)
+        h: list of N human labels, 1 for prefer A, -1 for prefer B, 0 for ties
+        m: list of N model predictions, can be obtained by Score(A) - Score(B)
     Output:
-        tau_star: accuracy-with-ties
+        acc_star: accuracy-with-ties
     """
     try:
         C, D, Th, Tm, Thm = suff_stats(h, m, -1)
         
         sorted_pairs = sorted(zip(h, m), key=lambda x: abs(x[1]))
         
-        tau_star = float('-inf')
+        acc_star = float('-inf')
         epsilon_star = 0
         epsilon_curr = -1
 
-        current_thresholds = {
+        current_stat = {
             'C': C, 'D': D, 'Th': Th, 'Tm': Tm, 'Thm': Thm
         }
         # print(current_thresholds)
         for hi, mi in sorted_pairs:
             # update the statistics by removing the current pair
             if hi == 0 and abs(mi) < epsilon_curr:
-                current_thresholds['Thm'] -= 1
+                current_stat['Thm'] -= 1
             elif hi == 0:
-                current_thresholds['Th'] -= 1
+                current_stat['Th'] -= 1
             elif abs(mi) < epsilon_curr:
-                current_thresholds['Tm'] -= 1
+                current_stat['Tm'] -= 1
             elif hi * mi > 0:
-                current_thresholds['C'] -= 1
+                current_stat['C'] -= 1
             else:
-                current_thresholds['D'] -= 1
+                current_stat['D'] -= 1
 
             # update the epsilon value
             epsilon_curr = abs(mi)
 
             # update the statistics by adding the current pair
             if hi == 0 and abs(mi) <= epsilon_curr:
-                current_thresholds['Thm'] += 1
+                current_stat['Thm'] += 1
             elif hi == 0:
-                current_thresholds['Th'] += 1
+                current_stat['Th'] += 1
             elif abs(mi) <= epsilon_curr:
-                current_thresholds['Tm'] += 1
+                current_stat['Tm'] += 1
             elif hi * mi > 0:
-                current_thresholds['C'] += 1
+                current_stat['C'] += 1
             else:
-                current_thresholds['D'] += 1
+                current_stat['D'] += 1
 
             # calculate the new tau value
-            tau_curr = calc_tau(**current_thresholds)
+            acc_curr = calc_acc(**current_stat)
 
-            if tau_curr > tau_star:
-                tau_star = tau_curr
+            if acc_curr > acc_star:
+                acc_star = acc_curr
                 epsilon_star = epsilon_curr
             # print(current_thresholds)
         # print("epsilon_star:", epsilon_star)
-        return tau_star
+        return acc_star
     except Exception as e:
         print("Error in tie_calibration:", e)
         return 0
     
 
 def calc_accuracy_without_ties(h, m):
+    """
+    Input:
+        h: list of N human labels, 1 for prefer A, -1 for prefer B, 0 for ties
+        m: list of N model predictions, can be obtained by Score(A) - Score(B)
+    Output:
+        acc_star: accuracy-without-ties
+    """
     C, D, Th, Tm, Thm = suff_stats(h, m, -1)
-    return C / (C + D)
+    return C / (C + D + Tm)
+
+
+if __name__ == "__main__":
+    h = [1, -1, 0, 1, -1, 0, 1, -1, 0]
+    
+    scores_A = [0.9, -0.7, 0.1, 0.8, -0.6, 0.2, 0.7, -0.5, 0.3]
+    scores_B = [0.1, -0.8, 0.5, 0.5, -0.3, 0.3, 0.4, -0.4, 0.4]
+    m = [a - b for a, b in zip(scores_A, scores_B)]
+
+    print("Accuracy with ties:", calc_accuracy_with_ties(h, m))
+    print("Accuracy without ties:", calc_accuracy_without_ties(h, m))
